93505f2c4c8cde4a010b37e4495d53b24320abde
duncan <dpelan3@gmail.com>
2022-06-07 16:01:08 +0200

    check empties in grid toegevoegd, gaat nu een buur functie van een coordinaat checken

--- grid.py
+++ grid.py
@@ -2,12 +2,14 @@ from car import Car
 #from loader import loader
 
 class Grid():
-    def __init__(self, size = 7):
+    def __init__(self, size):
         self._grid = []
         for i in range(size):
             self._grid.append(size * ['*'])
         self._cars = {}
         self._size = size
+        # is true when red car is at finish from move
+        self._win = False
 
     def add_car(self, name, orientation, col, row, length):
         car = Car(name, orientation, col, row, length, self._size)
@@ -17,7 +19,7 @@ class Grid():
                 self._grid[row][col + i] = name
             elif orientation == 'V':
                 self._grid[row + i][col] = name
-
+                                                                                                                                                                                                      
 
     def move(self, name, distance):
         car = self._cars[name]
@@ -36,16 +38,26 @@ class Grid():
                 self._grid[row + i + distance][col] = name
 
         if name == 'X':
-            car.win()
+            self._win = car.win()
+
+    def check_empties(self):
+        list_of_empties = []
+        for y in range(self._size):
+            for x in range(self._size):
+                element = self._grid[y][x] 
+                if element == "*":
+                    # print(f"{x, y} are coordinates for * ")
+                    list_of_empties.append(element)
+        return list_of_empties
+
+    
 
     def print_grid(self):
         for row in self._grid:
             print(''.join(row))
 
 
-
-
 if __name__ == '__main__':
     grid = Grid()
-    grid.add_car('X', 'V', 3, 0, 2)
-    grid.print_grid()
+    # grid.add_car('X', 'V', 3, 0, 2)
+    # grid.print_grid()
