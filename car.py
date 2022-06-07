bc4fd53b64efb29a427f6f9ef80ba1a6fd3ce125
duncan <dpelan3@gmail.com>
2022-06-07 14:02:32 +0200

    grid_size wordt nu meegegeven aan de auto om de winconditie te checken

--- car.py
+++ car.py
@@ -1,10 +1,12 @@
 class Car():
-    def __init__(self, name, orientation, col, row, length) -> None:
+    def __init__(self, name, orientation, col, row, length, grid_size) -> None:
         self._name = name
         self._orientation = orientation
         self._col = col
         self._row = row
         self._length = length
+        # give the columns of the grid for the win condition
+        self._grid_size = grid_size
 
     # gives the coordinates of the car as a list of (x, y)
     def coordinates(self):
@@ -16,10 +18,7 @@ class Car():
     def win(self):
         # red car is denoted by X
         if self._name == "X":
-            # currently do it like this, but the last column needs to be added from the grid initialization
-            # either by a super from the grid, or by giving the lenght of the grid as initialization variable
-            self._win_coordinates = 6 # grid._columns
-            if self._col == self._win_coordinates:
+            if self._col == self._grid_size:
                 return True
             else:
                 return False
@@ -29,5 +28,5 @@ class Car():
 # # possibly a subclass for a red car is quicker as we only loop over 1 car rather than all cars  
 # # depends on our grid model for checking the win condition
 # class RedCar(Car):
-#     def __init__(self, name, orientation, col, row, length) -> None:
-#         super().__init__(name, orientation, col, row, length)
+#     def __init__(self, name, orientation, col, row, length, grid_size) -> None:
+#         super().__init__(name, orientation, col, row, length, grid_size)
