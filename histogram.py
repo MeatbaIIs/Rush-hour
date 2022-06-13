477694a9f5842f659947f7043321e04678161264
duncan <dpelan3@gmail.com>
2022-06-13 11:59:10 +0200

    we gaan vanaf hier de github herstructeren zoals ze in het code college doen. Er is een main, en dan een folder met codes met folders voor algoritmes, de classes en andere code zoals helpers. loader wordt nu helper

---
+++ histogram.py
@@ -0,0 +1,33 @@
+"""
+Code to make histograms from codes and alogrithms to dertemine what a feasable upper bound for the number of steps is
+
+"""
+from enum import auto
+from grid import Grid
+from helpers import loader
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+file_name = "data/Rushhour12x12_7.csv"
+grid = loader(file_name)
+grid.print_grid()
+
+N = 100
+print(f"going to solve previous grid {N} times")
+
+counts = []
+counts_2 = []
+
+for i in range(N):
+    grid = loader(file_name)
+    tries = grid.random_algorythm()
+    tries_2 = grid.other_random_algorithm()
+
+    counts.append(tries)
+    counts_2.append(tries_2)
+
+fig, ax = plt.subplots(figsize =(10, 7))
+ax.hist(counts, bins=20)
+plt.show()
+
