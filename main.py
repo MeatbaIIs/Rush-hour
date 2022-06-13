04cc8e11e51ca0f86c4f706e132dcc1137b11bd5
duncan <dpelan3@gmail.com>
2022-06-13 14:49:56 +0200

    niet iets gedaan

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ Takes Rush hour puzzle in the command line and solves it.
 Usage: main.py [PUZZLE_NAME.CSV]
 """
 
-import csv
 from code.algorithms.depth_first import Depth_first
 from code.classes.car import Car
 from code.classes.grid import Grid
 from code.helpers import loader, save_solution
 import argparse
-import re
 
 
 def main(input_file_name):
