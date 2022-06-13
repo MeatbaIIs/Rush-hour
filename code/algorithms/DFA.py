3d0f8f198c6c24aec735daa27ffb8e7a2bb8b693
duncan <dpelan3@gmail.com>
2022-06-13 14:43:40 +0200

    leons dfa in de andere folder gezet

---
+++ code/algorithms/DFA.py
@@ -0,0 +1,16 @@
+"""
+Anneloes'en DUncans wondercode
+"""
+
+from matplotlib.pyplot import cla
+from classes.grid import Grid
+from helpers import loader
+
+class DepthFirst:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        # loopt tot de grid opgelost is
+        pass
+
