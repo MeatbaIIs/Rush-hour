0953751f47f42d2c7ce5b192152e6a9806d47384
duncan <dpelan3@gmail.com>
2022-06-14 11:53:00 +0200

    pseudocode is af

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -28,25 +28,25 @@ class Grid():
             elif orientation == 'V':
                 self._grid[y + i][x] = name
 
-    def random_algorythm(self):
-        """Move a random car randomly and check for the win condition"""
-        random_car = random.choice(list(self._cars.keys()))
+    # def random_algorythm(self):
+    #     """Move a random car randomly and check for the win condition"""
+    #     random_car = random.choice(list(self._cars.keys()))
 
-        steps = 0
-        # t = time.time()
-        while not self.win():
-            # get the possible moves and pick a random one
-            moves = self.possible_moves(random_car)
+    #     steps = 0
+    #     # t = time.time()
+    #     while not self.win():
+    #         # get the possible moves and pick a random one
+    #         moves = self.possible_moves(random_car)
 
-            if moves:
-                steps += 1
-                random_move = random.choice(moves)
-                self.move(random_car, random_move)
-            # pick a new random car
-            random_car = random.choice(list(self._cars.keys()))
-        # print(f"Yay, solved in {steps} steps and {time.time() - t} seconds")
+    #         if moves:
+    #             steps += 1
+    #             random_move = random.choice(moves)
+    #             self.move(random_car, random_move)
+    #         # pick a new random car
+    #         random_car = random.choice(list(self._cars.keys()))
+    #     # print(f"Yay, solved in {steps} steps and {time.time() - t} seconds")
 
-        return steps
+    #     return steps
 
     def random_step(self):
         """Move a random car randomly and check for the win condition"""
