8fc3df001d16bcbb6b7528f5b33485ab18595a6f
ALvanSchaik <alvanschaik@LAPTOP-3VG4O2RA.localdomain>
2022-06-29 16:10:34 +0200

    restructuring main_an for experiment

--- main_an.py
+++ main_an.py
@@ -24,66 +24,8 @@ import matplotlib.pyplot as plt
 
 
 def main(input_file_name):
-    # grid = loader(input_file_name)
-    # start_time = time.perf_counter()
-
-    # depth first
-    # depth_first = dfs(grid)
-    # solution = depth_first.run()
-
-    # random
-    # random_alg = Random(grid)
-    # solution, time_sth = random_alg.random_algorithm()
-
-    # breadth first
-    # breadth_first = BreadthFirst(grid)
-    # solution = breadth_first.run()
-
-    # Random optimizing
-    # randopt = ImprovingRandom(input_file_name)
-    # solution = randopt.run()
-
-    # Beam Search
-    # beam_search = BeamSearch(grid)
-    # solution = beam_search.run()
-
-    # depth first Duncan
-    # depth_first = DepthFirst('data/Rushhour6x6_1.csv')
-    # solution = depth_first
-
-    # end_time = time.perf_counter()
-    # sol_len = len(solution)
-    # duration = round((end_time - start_time)/60, 2)
-
-    # print(
-    #     f'Algorithm took around {duration} minutes and found solution of {sol_len} steps')
-
-    # IA = Improving_algorithm(grid, solution)
-    # new_solution = IA.run()
-    # print(
-    #     f'Improving algorithm lead to a solution of {len(new_solution)} steps.')
-
-    # Take out loops
-    # tol = TakeOutLoops(grid, solution)
-    # new_solution = tol.run()
-    # print(f'solution length after take out loops: {len(new_solution)}')
-
-    # histogram(input_file_name)
-    # dfs.run(grid)
-    # print('found a solution!')
-    #algorithm = Random(grid)
-    # print(algorithm.other_random_algorithm())
-    # dfs.run(grid)
-    # print('found a solution!')
-    # algorithm = dfs(grid)
-    # algorithm.run()
-
-    # algorithm = DF("data/Rushhour6x6_1.csv")
-    # algorithm.run()
-
-    # output_file_name = input_file_name.rstrip(".csv") + "_solution.csv"
-    # solution_to_csv(solution, output_file_name)
-
+    # METHOD GENERATE_SOLUTIONS
+    # generates 10 solutions with improving random set to time = 60 seconds
     # Generate random solutions and save
     N = 10
     puzzles = [
@@ -100,113 +42,113 @@ def main(input_file_name):
         "RU",
         "ALL"
     ]
-    # results = pd.DataFrame(columns=["puzzle", "iteration", "steps", "time"])
-    # for puzzle in puzzles:
-    #     stable_grid = loader(puzzle)
-    #     for i in range(N):
-    #         print(f'Puzzle {puzzle} iteration {i}')
-    #         grid = copy.deepcopy(stable_grid)
-    #         start_time = time.perf_counter()
+    results = pd.DataFrame(columns=["puzzle", "iteration", "steps", "time"])
+    for puzzle in puzzles:
+        stable_grid = loader(puzzle)
+        for i in range(N):
+            print(f'Puzzle {puzzle} iteration {i}')
+            grid = copy.deepcopy(stable_grid)
+            start_time = time.perf_counter()
 
-    #         IR = ImprovingRandom(grid)
-    #         solution = IR.run_improving("time", 60)
-    #         end_time = time.perf_counter()
+            IR = ImprovingRandom(grid)
+            solution = IR.run_improving("time", 60)
+            end_time = time.perf_counter()
 
-    #         duration = end_time - start_time
+            duration = end_time - start_time
 
-    #         steps = len(solution)
-    #         iteration = i
-    #         puzzle_num = puzzle
-    #         results.loc[len(results)] = [puzzle_num,
-    #                                      iteration, steps, duration]
+            steps = len(solution)
+            iteration = i
+            puzzle_num = puzzle
+            results.loc[len(results)] = [puzzle_num,
+                                         iteration, steps, duration]
 
-    #         output_file_name = "data/random" + puzzle.rstrip(
-    #             ".csv").lstrip("data") + "_IR_solution_" + str(i) + ".csv"
-    #         solution_to_csv(solution, output_file_name)
+            output_file_name = "data/random" + puzzle.rstrip(
+                ".csv").lstrip("data") + "_IR_solution_" + str(i) + ".csv"
+            solution_to_csv(solution, output_file_name)
 
-    # print(results)
-    # average_time = results["time"].mean()
-    # print(f'average time was {average_time}')
-    # average_steps = results["steps"].mean()
-    # print(f'average steps was {average_steps}')
+    print(results)
+    average_time = results["time"].mean()
+    print(f'average time was {average_time}')
+    average_steps = results["steps"].mean()
+    print(f'average steps was {average_steps}')
 
-    # results.to_csv("data/random/impro_random_results.csv", index=False)
+    # METHOD OPTIMIZE
+    # Optimize the previously generated solutions with TakeOutLoops, RemoveUseless, and both each for 3 iterations
+    results.to_csv("data/random/impro_random_results.csv", index=False)
 
-    # iterative_results = pd.DataFrame(
-    #     columns=["puzzle", "solution", "iteration", "algorithm", "steps", "time"])
-    # for puzzle in puzzles:
+    iterative_results = pd.DataFrame(
+        columns=["puzzle", "solution", "iteration", "algorithm", "steps", "time"])
+    for puzzle in puzzles:
 
-    #     grid = loader(puzzle)
+        grid = loader(puzzle)
 
-    #     for i in range(N):
+        for i in range(N):
 
-    #         input_file_name = "data/random" + \
-    #             puzzle.rstrip(".csv").lstrip("data") + \
-    #             "_IR_solution_" + str(i) + ".csv"
-    #         solution = load_solution(input_file_name)
+            input_file_name = "data/random" + \
+                puzzle.rstrip(".csv").lstrip("data") + \
+                "_IR_solution_" + str(i) + ".csv"
+            solution = load_solution(input_file_name)
 
-    #         for j in range(3):
+            for j in range(3):
 
-    #             print(f'Optimizing a solution of {len(solution)} steps.')
+                print(f'Optimizing a solution of {len(solution)} steps.')
 
-    #             for algorithm in algorithms:
+                for algorithm in algorithms:
 
-    #                 start_time = time.perf_counter()
-    #                 if algorithm == "TOL":
-    #                     print('Taking out loops.')
-    #                     TOL = TakeOutLoops(grid, solution)
-    #                     new_solution = TOL.run()
-    #                 elif algorithm == "RU":
-    #                     print('Seeing if any useless steps can be left out.')
-    #                     RU = RemoveUseless(grid, solution)
-    #                     new_solution = RU.run()
-    #                 # elif algorithm == "BFI":
-    #                 #     print('Iterating with Breadth First')
-    #                 #     BFI = BreadthFirstIter(grid, solution)
-    #                 #     new_solution = BFI.run()
-    #                 elif algorithm == "ALL":
-    #                     print('Taking out loops.')
-    #                     TOL = TakeOutLoops(grid, solution)
-    #                     new_solution = TOL.run()
-    #                     print('Seeing if any useless steps can be left out.')
-    #                     RU = RemoveUseless(grid, new_solution)
-    #                     new_solution = RU.run()
-    #                 #     print('Iterating with Breadth First')
-    #                 #     BFI = BreadthFirstIter(grid, new_solution)
-    #                 #     new_solution = BFI.run()
+                    start_time = time.perf_counter()
+                    if algorithm == "TOL":
+                        print('Taking out loops.')
+                        TOL = TakeOutLoops(grid, solution)
+                        new_solution = TOL.run()
+                    elif algorithm == "RU":
+                        print('Seeing if any useless steps can be left out.')
+                        RU = RemoveUseless(grid, solution)
+                        new_solution = RU.run()
 
-    #                 duration = time.perf_counter() - start_time
+                    elif algorithm == "ALL":
+                        print('Taking out loops.')
+                        TOL = TakeOutLoops(grid, solution)
+                        new_solution = TOL.run()
+                        print('Seeing if any useless steps can be left out.')
+                        RU = RemoveUseless(grid, new_solution)
+                        new_solution = RU.run()
 
-    #                 print(f'The solution is {len(new_solution)} steps.')
-    #                 filename = input_file_name.rstrip(
-    #                     ".csv") + "_optimized.csv"
-    #                 solution_to_csv(new_solution, filename)
-    #                 print(filename)
+                    duration = time.perf_counter() - start_time
 
-    #                 steps = len(new_solution)
-    #                 iteration = i
-    #                 puzzle_num = puzzle
-    #                 iterative_results.loc[len(iterative_results)] = [puzzle_num, j,
-    #                                                                  iteration, algorithm, steps, duration]
+                    print(f'The solution is {len(new_solution)} steps.')
+                    filename = input_file_name.rstrip(
+                        ".csv") + "_optimized.csv"
+                    solution_to_csv(new_solution, filename)
+                    print(filename)
 
-    # iterative_results.to_csv("data/random/iterative_results.csv", index=False)
+                    steps = len(new_solution)
+                    iteration = i
+                    puzzle_num = puzzle
+                    iterative_results.loc[len(iterative_results)] = [puzzle_num, j,
+                                                                     iteration, algorithm, steps, duration]
 
-    # # Plots
-    # results = pd.read_csv("data/random/impro_random_results.csv")
-    # means = results.groupby("puzzle").mean()
-    # xs = pd.Series([7, 1, 2, 3, 4, 5, 6])
-    # puzzles = means.index
-    # steps = means["steps"]
+    iterative_results.to_csv("data/random/iterative_results.csv", index=False)
+
+    # METHOD PLOT_IMPROVINGRANDOM
+    # Plot the length of the solutions generated with ImprovingRandom
+    results = pd.read_csv("data/random/impro_random_results.csv")
+    means = results.groupby("puzzle").mean()
+    xs = pd.Series([7, 1, 2, 3, 4, 5, 6])
+    puzzles = means.index
+    steps = means["steps"]
     fig, ax = plt.subplots()
 
-    # ax.bar(xs, steps)
-    # ax.set_xticklabels([0, 1, 2, 3, 4, 5, 6, 7])
+    ax.bar(xs, steps)
+    ax.set_xticklabels([0, 1, 2, 3, 4, 5, 6, 7])
 
-    # ax.set_xlabel('Puzzle number', labelpad=15)
-    # ax.set_ylabel('Steps', labelpad=15)
-    # ax.set_title(
-    #     'Solution length after running Improving Random for 1 minute', pad=15)
-    # plt.show()
+    ax.set_xlabel('Puzzle number', labelpad=15)
+    ax.set_ylabel('Steps', labelpad=15)
+    ax.set_title(
+        'Solution length after running Improving Random for 1 minute', pad=15)
+    plt.show()
+
+    # METHOD PLOT_ITERATION_STEPS
+    # Plot the length of the solutions after optimization
 
     solution_results = pd.read_csv("data/random/iterative_results.csv")
     # print(solution_results)
@@ -214,6 +156,7 @@ def main(input_file_name):
     means = solution_results.groupby("puzzle").mean()
     print(means)
     puzzles = means.index
+    fig, ax = plt.subplots()
 
     xs = pd.Series([7, 1, 2, 3, 4, 5, 6])
 
@@ -222,45 +165,70 @@ def main(input_file_name):
     tol_sd = filtered.groupby("puzzle").std()
     tol_steps_sd = tol_sd['steps']
     tol_steps = tol['steps']
-    tol_time = tol['time']
-    tol_time_sd = tol_sd['time']
 
     filtered = solution_results[solution_results.algorithm == "RU"]
     ru = filtered.groupby("puzzle").mean()
     ru_sd = filtered.groupby("puzzle").std()
     ru_steps_sd = ru_sd['steps']
     ru_steps = ru['steps']
+
+    filtered = solution_results[solution_results.algorithm == "ALL"]
+    al = filtered.groupby("puzzle").mean()
+    all_sd = filtered.groupby("puzzle").std()
+    all_steps_sd = all_sd['steps']
+    all_steps = al['steps']
+
+    bar_width = 0.2
+
+    ax.bar(xs - bar_width, tol_steps, width=bar_width)
+    ax.bar(xs, ru_steps, width=bar_width)
+    ax.bar(xs + bar_width, all_steps, width=bar_width)
+    ax.errorbar(xs - bar_width, tol_steps,
+                yerr=tol_steps_sd, fmt="o", color='black')
+    ax.errorbar(xs, ru_steps,
+                yerr=ru_steps_sd, fmt="o", color='black')
+    ax.errorbar(xs + bar_width, all_steps,
+                yerr=all_steps_sd, fmt="o", color='black')
+    ax.set_xticklabels([0, 1, 2, 3, 4, 5, 6, 7])
+
+    ax.set_xlabel('Puzzle number', labelpad=15)
+    ax.set_ylabel('Steps', labelpad=15)
+    ax.legend(['TakeOutLoops', 'RemoveUseless', 'Both'])
+    ax.set_title(
+        'Solution length after optimizing with different algorithms', pad=15)
+
+    plt.show()
+
+    # METHOD PLOT_ITERATION_TIME
+    # Plot the duration of the optimization
+    solution_results = pd.read_csv("data/random/iterative_results.csv")
+
+    means = solution_results.groupby("puzzle").mean()
+    print(means)
+    puzzles = means.index
+    fig, ax = plt.subplots()
+
+    xs = pd.Series([7, 1, 2, 3, 4, 5, 6])
+
+    filtered = solution_results[solution_results.algorithm == "TOL"]
+    tol = filtered.groupby("puzzle").mean()
+    tol_sd = filtered.groupby("puzzle").std()
+    tol_time = tol['time']
+    tol_time_sd = tol_sd['time']
+
+    filtered = solution_results[solution_results.algorithm == "RU"]
+    ru = filtered.groupby("puzzle").mean()
+    ru_sd = filtered.groupby("puzzle").std()
     ru_time = ru['time']
     ru_time_sd = ru_sd['time']
 
     filtered = solution_results[solution_results.algorithm == "ALL"]
     al = filtered.groupby("puzzle").mean()
     all_sd = filtered.groupby("puzzle").std()
-    all_steps_sd = all_sd['steps']
     all_time_sd = all_sd['time']
-    all_steps = al['steps']
     all_time = al['time']
 
     bar_width = 0.2
-
-    # ax.bar(xs - bar_width, tol_steps, width=bar_width)
-    # ax.bar(xs, ru_steps, width=bar_width)
-    # ax.bar(xs + bar_width, all_steps, width=bar_width)
-    # ax.errorbar(xs - bar_width, tol_steps,
-    #             yerr=tol_steps_sd, fmt="o", color='black')
-    # ax.errorbar(xs, ru_steps,
-    #             yerr=ru_steps_sd, fmt="o", color='black')
-    # ax.errorbar(xs + bar_width, all_steps,
-    #             yerr=all_steps_sd, fmt="o", color='black')
-    # ax.set_xticklabels([0, 1, 2, 3, 4, 5, 6, 7])
-
-    # ax.set_xlabel('Puzzle number', labelpad=15)
-    # ax.set_ylabel('Steps', labelpad=15)
-    # ax.legend(['TakeOutLoops', 'RemoveUseless', 'Both'])
-    # ax.set_title(
-    #     'Solution length after optimizing with different algorithms', pad=15)
-
-    # plt.show()
 
     ax.bar(xs - bar_width, tol_time, width=bar_width)
     ax.bar(xs, ru_time, width=bar_width)
